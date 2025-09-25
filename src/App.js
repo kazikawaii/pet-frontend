b8f5e24dd1182aeb5ad927c336f2cb922a2d5453
kazikawaii <kazikawaii80@gmail.com>
2025-09-25 23:03:40 +0500

    Kamoshcommit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
 
         <a
-          className="App-link"
+          className="App"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
